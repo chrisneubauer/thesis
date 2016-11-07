c21bb07644781ec8063eb6bfe1f1435c5db535f1
Christoph Neubauer <neubauer.office@gmail.com>
2016-11-07 15:08:26 -0200

    added interesting literature for ocr approaches

---
+++ Application/src/main/java/de/cneubauer/util/config/Cfg.java
@@ -0,0 +1,7 @@
+package de.cneubauer.util.config;
+
+/**
+ * Created by Christoph Neubauer on 07.11.2016.
+ */
+public enum Cfg {
+}
---
+++ Application/src/main/java/de/cneubauer/util/enumeration/AppLang.java
@@ -0,0 +1,7 @@
+package de.cneubauer.util.enumeration;
+
+/**
+ * Created by Christoph Neubauer on 07.11.2016.
+ */
+public enum AppLang {
+}
