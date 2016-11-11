969e959d6a1238a546c713e461595f07bf76c2ea
Christoph Neubauer <neubauer.office@gmail.com>
2016-11-11 21:01:33 -0200

    extended neuron

--- Application/src/main/java/de/cneubauer/ann/impl/NeuronImpl.java
+++ Application/src/main/java/de/cneubauer/ann/impl/NeuronImpl.java
@@ -1,9 +1,66 @@
 package de.cneubauer.ann.impl;
 
 import de.cneubauer.ann.Neuron;
+import de.cneubauer.ann.NeuronStrategy;
 
 /**
  * Created by Christoph Neubauer on 11.11.2016.
  */
 public class NeuronImpl implements Neuron {
+    @Override
+    public void setStrategy(NeuronStrategy ns) {
+        
+    }
+
+    @Override
+    public NeuronStrategy getStrategy() {
+        return null;
+    }
+
+    @Override
+    public void updateOutputValue() {
+
+    }
+
+    @Override
+    public void updateDelta(double errorFactor) {
+
+    }
+
+    @Override
+    public void updateFreeParams() {
+
+    }
+  /*  'Calculate the error value
+    Public Sub UpdateDelta(ByVal errorFactor As Single) Implements _
+    NeuralFramework.INeuron.UpdateDelta
+
+    If _strategy Is Nothing Then
+    Throw New StrategyNotInitializedException("", Nothing)
+
+    'Error factor is found and passed to this
+    DeltaValue = Strategy.FindDelta(OutputValue, errorFactor)
+    End Sub
+
+'Calculate the output
+    Public Sub UpdateOutput() _
+    Implements NeuralFramework.INeuron.UpdateOutput
+
+    If _strategy Is Nothing Then
+    Throw New StrategyNotInitializedException("..", Nothing)
+
+    Dim netValue As Single = Strategy.FindNetValue(Inputs, BiasValue)
+    OutputValue = Strategy.Activation(netValue)
+    End Sub
+
+'Calculate the free parameters
+    Public Sub UpdateFreeParams() _
+    Implements NeuralFramework.INeuron.UpdateFreeParams
+
+    If _strategy Is Nothing Then
+    Throw New StrategyNotInitializedException("..", Nothing)
+
+    BiasValue = Strategy.FindNewBias(BiasValue, DeltaValue)
+            Strategy.UpdateWeights(Inputs, DeltaValue)
+*/
 }
