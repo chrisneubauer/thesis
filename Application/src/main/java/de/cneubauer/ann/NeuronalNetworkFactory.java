c6a8cf3a9efebfba16fcb005e30cd70ab950b294
Christoph Neubauer <neubauer.office@gmail.com>
2016-11-11 21:21:24 -0200

    added several signatures to the ann

--- Application/src/main/java/de/cneubauer/ann/NeuronalNetworkFactory.java
+++ Application/src/main/java/de/cneubauer/ann/NeuronalNetworkFactory.java
@@ -1,7 +1,10 @@
 package de.cneubauer.ann;
 
+import java.util.ArrayList;
+
 /**
  * Created by Christoph Neubauer on 11.11.2016.
  */
 public interface NeuronalNetworkFactory {
+    void createNetwork(ArrayList<Long> neurons);
 }
