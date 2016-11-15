5588441b37c105b041bd4be6a13132491e748fe2
Christoph Neubauer <neubauer.office@gmail.com>
2016-11-15 16:36:43 -0200

    adjusted bos, hbms and sql generator

--- Application/src/main/java/de/cneubauer/domain/bo/Account.java
+++ Application/src/main/java/de/cneubauer/domain/bo/Account.java
@@ -4,4 +4,40 @@ package de.cneubauer.domain.bo;
  * Created by Christoph Neubauer on 15.11.2016.
  */
 public class Account {
+    private int id;
+    private String accountNo;
+    private String name;
+    private AccountType type;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAccountNo() {
+        return accountNo;
+    }
+
+    public void setAccountNo(String accountNo) {
+        this.accountNo = accountNo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public AccountType getType() {
+        return type;
+    }
+
+    public void setType(AccountType type) {
+        this.type = type;
+    }
 }
--- Application/src/main/java/de/cneubauer/util/SQLGenerator.java
+++ Application/src/main/java/de/cneubauer/util/SQLGenerator.java
@@ -1,7 +1,42 @@
 package de.cneubauer.util;
 
+import java.io.*;
+
 /**
  * Created by Christoph Neubauer on 15.11.2016.
+ * Used for generating sql sequences
  */
 public class SQLGenerator {
+    public void createAccountSQL() {
+        File text = new File("..\\Accounting\\KontenUTF8.txt");
+        File output = new File("..\\Accounting\\InsertAccountData.sql");
+        try {
+            InputStream in = new FileInputStream(text);
+            BufferedReader r = new BufferedReader(new InputStreamReader(in));
+            String line = r.readLine();
+            BufferedWriter w = new BufferedWriter(new FileWriter(output));
+            while (line != null) {
+                System.out.println(line);
+                //String[] parts = line.split(" ");
+                //int completeLines = parts.length - 1;
+                String accNo = line.substring(0, 4); //parts[0];
+                String accName = line.substring(5, line.length() - 2);// "";
+                /*for (int i = 1; i < completeLines; i++) {
+                    accName += parts[i];
+                }*/
+                String accType = line.substring(line.length() - 1, line.length());//parts[completeLines];
+                String sqlString = "INSERT INTO Account (AccountNo, Name, AccountType_Id) VALUES (" +
+                        "\"" + accNo + "\", \"" + accName + "\", " + accType + ");";
+                System.out.println("Writing: " + sqlString);
+                w.write(sqlString);
+                w.newLine();
+                line = r.readLine();
+            }
+            w.flush();
+            w.close();
+            r.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
