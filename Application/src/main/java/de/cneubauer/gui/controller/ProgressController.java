7b23ac19c94c7270d81c6c5eae6456d5c73ee532
Christoph Neubauer <neubauer.office@gmail.com>
2016-11-29 12:05:51 -0200

    added processresult and processlistcontroller, scanstatus enum and removed pdf scanner test

---
+++ Application/src/main/java/de/cneubauer/gui/controller/ProgressController.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by Christoph Neubauer on 25.11.2016.
+ */
+public class ProgressController {
+}
---
+++ Application/src/main/java/de/cneubauer/util/enumeration/ScanStatus.java
@@ -0,0 +1,7 @@
+package de.cneubauer.util.enumeration;
+
+/**
+ * Created by Christoph Neubauer on 29.11.2016.
+ */
+public enum ScanStatus {
+}
