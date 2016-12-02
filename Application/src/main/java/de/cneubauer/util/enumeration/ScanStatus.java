f49d67c18c7d340d3dcbfb6d53de076f983ce530
Christoph Neubauer <neubauer.office@gmail.com>
2016-12-02 13:51:20 -0200

    added ISSUE State

--- Application/src/main/java/de/cneubauer/util/enumeration/ScanStatus.java
+++ Application/src/main/java/de/cneubauer/util/enumeration/ScanStatus.java
@@ -2,8 +2,13 @@ package de.cneubauer.util.enumeration;
 
 /**
  * Created by Christoph Neubauer on 29.11.2016.
+ * State of Scan Result
+ * OK: Invoice successfully scanned, enough information found for saving in db
+ * ISSUE: Invoice successfully scanned, issues with information which make it impossible to save
+ * ERROR: Issues during scan, has to be reviewed manually
  */
 public enum ScanStatus {
     OK,
-    ERROR
+    ERROR,
+    ISSUE
 }
