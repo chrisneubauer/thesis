1a5721592151ca8aed784a349714878a3cfab84d
Christoph Neubauer <neubauer.office@gmail.com>
2016-12-12 11:51:21 -0200

    added a worker thread for tesseract execution as well as an partitioner who splits the image in important sections

---
+++ Application/src/main/java/de/cneubauer/ocr/ImagePartitioner.java
@@ -0,0 +1,7 @@
+package de.cneubauer.ocr;
+
+/**
+ * Created by Christoph Neubauer on 12.12.2016.
+ */
+public class ImagePartitioner {
+}
---
+++ Application/src/main/java/de/cneubauer/ocr/tesseract/TesseractWorker.java
@@ -0,0 +1,7 @@
+package de.cneubauer.ocr.tesseract;
+
+/**
+ * Created by Christoph Neubauer on 09.12.2016.
+ */
+public class TesseractWorker {
+}
