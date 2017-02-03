578b91e2f9ebecaa27ef6d7fd4e4c648ff42de32
Christoph Neubauer <neubauer.office@gmail.com>
2017-02-02 18:34:18 -0700

    added machine learning package and model classes

---
+++ Application/src/main/java/de/cneubauer/ml/LearningService.java
@@ -0,0 +1,7 @@
+package de.cneubauer.ml;
+
+/**
+ * Created by Christoph Neubauer on 02.02.2017.
+ */
+public class LearningService {
+}
---
+++ Application/src/test/java/de/cneubauer/ml/ModelWriterTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Christoph Neubauer on 02.02.2017.
+ */
+public class ModelWriterTest {
+
+}
