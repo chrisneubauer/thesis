e5a9aa002961fdf8ba40cb65b0ef6e1dd1ae812a
Christoph Neubauer <neubauer.office@gmail.com>
2017-02-05 17:25:20 -0700

    added javadoc in connector package

--- Application/src/main/java/de/cneubauer/database/MySQLConnector.java
+++ Application/src/main/java/de/cneubauer/database/MySQLConnector.java
@@ -1,14 +1,13 @@
 package de.cneubauer.database;
 
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
-import de.cneubauer.util.config.Cfg;
 import de.cneubauer.util.config.ConfigHelper;
 
 import java.sql.Connection;
 
 /**
  * Created by Christoph Neubauer on 05.10.2016.
- *
+ * Connector class to enable access to a MySQL database
  */
 public class MySQLConnector {
     private final String USERNAME;
@@ -17,6 +16,14 @@ public class MySQLConnector {
     private final String DATABASE;
     private final int PORT;
 
+    /**
+     *
+     * @param srv  the servername where the mysql database is
+     * @param db  the name of the database to be used
+     * @param usr  the username which is used to access the db
+     * @param pw  the password for the corresponding user
+     * @param prt  the port of the mysql databse (default: 3306)
+     */
     public MySQLConnector(String srv, String db, String usr, String pw, int prt) {
         this.USERNAME = usr;
         this.PW = pw;
@@ -33,6 +40,10 @@ public class MySQLConnector {
         this.PORT = ConfigHelper.getDBPort();
     }
 
+    /**
+     * Connects to the mysql database using the given credentials
+     * @return  returns an open connection to the mysql data source
+     */
     public Connection connect() {
         MysqlDataSource dataSource = new MysqlDataSource();
         dataSource.setUser(this.getUSERNAME());
@@ -45,7 +56,7 @@ public class MySQLConnector {
         try {
             con = dataSource.getConnection();
         } catch (Exception e) {
-            // try again
+            // try again just in case there was a problem with the internet connection
             try {
                 Thread.sleep(3000);
                 con = dataSource.getConnection();
