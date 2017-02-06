c15f593e2f0a52b7646ddf5e695111cb97cb5ef0
Christoph Neubauer <neubauer.office@gmail.com>
2017-02-05 17:07:50 -0700

    removed ANN and learning package due to new ml package

--- Application/src/main/java/de/cneubauer/ann/impl/NeuronalLayer.java
+++
@@ -1,7 +0,0 @@
-package de.cneubauer.ann.impl;
-
-/**
- * Created by Christoph Neubauer on 11.11.2016.
- */
-public class NeuronalLayer {
-}
--- Application/src/main/java/de/cneubauer/ann/training/NetworkTrainer.java
+++
@@ -1,7 +0,0 @@
-package de.cneubauer.ann.training;
-
-/**
- * Created by Christoph Neubauer on 11.11.2016.
- */
-public class NetworkTrainer {
-}
