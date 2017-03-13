279fee9552aebf2e27c074cf9ccd5be21caacd3e
Christoph Neubauer <neubauer.christoph@gmx.de>
2017-03-13 17:41:44 +0100

    removed additional data

--- Trainingsdata/createFolders.bat
+++
@@ -1,6 +0,0 @@
-FOR %%L IN (Arial, TimesNewRoman, Calibri, Helvetica, GillSans) DO (
-	if not exist %cd%\%%L md %cd%\%%L
-	FOR %%G IN (A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z) DO (
-		if not exist %cd%\%%L\%%G md %cd%\%%L\%%G
-	)
-)
